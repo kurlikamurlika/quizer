006c25e515a673abf2989e35580ba9880c3c7ee7
kurlikamurlika <alexkurlika@gmail.com>
2022-03-16 11:03:19 +0400

    first

--- quiz/models.py
+++ quiz/models.py
@@ -1,31 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-'''
-class User(models.Model):
-    first_name = models.CharField(max_length=200)
-    username = models.CharField(max_length=200, default="")
-    last_name = models.CharField(max_length=200)
-    user_email = models.EmailField(max_length=254)
-    user_password = models.CharField(max_length=200)
-    user_image = models.ImageField(upload_to=f'user_images/{username}/')
-    reg_date = models.DateTimeField('Дата регистрации')
-
-    def __str__(self):
-        return self.user_email
-'''
 
 class Quiz(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     quiz_name = models.CharField(max_length=200)
-
-    '''
-    def getUsername(self):
-        return self.user.username
-    username = getUsername
-    quiz_image = models.ImageField(upload_to=f'user_images/{username}/{quiz_name}/')
-    '''
-
     pub_date = models.DateTimeField('Publication date')
 
     def __str__(self):
@@ -49,7 +28,15 @@ class Answer(models.Model):
 class Play(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     points = models.IntegerField(default=0)
-    user = models.ForeignKey(User, on_delete=models. CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.quiz.quiz_name}_{self.user.username}_{self.points}'
+
+class Leaderboard(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    points = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.quiz.quiz_name}_{self.user.username}_{self.points}'
--- quiz/views.py
+++ quiz/views.py
@@ -88,7 +88,7 @@ def result(request, quiz_id):
     return render(request, 'quiz/result.html', context={'points': points, 'play_list': play_list})
 
 def create(request):
-    return render(request, 'quiz/create_test.html', context={})
+    return render(request, 'quiz/create.html', context={})
 
 def edit(request):
     quiz_name = request.POST['quiz_name']
@@ -98,40 +98,22 @@ def edit(request):
     answer_number = abs(int(request.POST['answer_number']))
     return render(request, 'quiz/edit.html', {'quiz_name': quiz_name, 'question_number': question_number, 'answer_number': answer_number, 'range_question': range(question_number), 'range_answer': range(answer_number)})
 
-def upload_test(request, question_number, answer_number):
-    for i in Quiz.objects.all():
-        quiz = i
-    quiz_id = quiz.id
-    for i in range(question_number):
-        question_text = request.POST[f'question_text{i}']
-        question = Question(quiz_id=quiz_id, question_text= question_text)
-        question.save()
-        for j in range(answer_number):
-            answer_text = request.POST[f'answer_text_{i}_{j}']
-            points = request.POST[f'answer_points_{i}_{j}']
-            answer = Answer(question_id=question.id, answer_text=answer_text, points=points)
-            answer.save()
-    return render(request, 'quiz/upload.html', context={'quiz_id': quiz_id})
-
 def upload(request):
-    quiz_name = request.POST['quiz_name']
+    quiz_name = request.POST[f'quiz_name']
     quiz = Quiz(user_id=request.user.id, quiz_name=quiz_name, pub_date=timezone.now())
     quiz.save()
-
     question_number = int(request.POST['question_number'])
-
     for i in range(1, question_number+1):
         question_text = request.POST[f'question_text_{i}']
         question = Question(quiz_id=quiz.id, question_text=question_text)
         question.save()
-        answer_number = request.POST[f'answer_{i}_number']
-        for j in range(1, answer_number + 1):
+        answer_number = int(request.POST[f'answer_{i}_number'])
+        for j in range(1, answer_number+1):
             answer_text = request.POST[f'answer_text_{i}_{j}']
-            given_points = request.POST[f'given_points_{i}_{j}']
-            answer = Answer(question_id=question.id, answer_text=answer_text, points=given_points)
+            points = request.POST[f'given_points_{i}_{j}']
+            answer = Answer(question_id=question.id, answer_text=answer_text, points=points)
             answer.save()
-
-    return render(request, 'quiz/upload.html', context={})
+    return render(request, 'quiz/upload.html', context={'quiz_id': quiz.id})
 
 def delete(request, quiz_id):
     quiz = Quiz.objects.get(pk=quiz_id)
